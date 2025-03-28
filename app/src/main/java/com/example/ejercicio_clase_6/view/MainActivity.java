36e07ef3fdc2af7c0cb522bb542e5951f812296e
Salomon <salomon.montilla.l@gmail.com>
2025-03-28 08:57:28 -0500

    agrega vista

--- app/src/main/java/com/example/ejercicio_clase_6/view/MainActivity.java
+++ app/src/main/java/com/example/ejercicio_clase_6/view/MainActivity.java
@@ -4,25 +4,34 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ListView;
 
+import com.example.ejercicio_clase_6.R;
+import com.example.ejercicio_clase_6.controller.EstudianteController;
 import com.example.ejercicio_clase_6.databinding.ActivityMainBinding;
+import com.example.ejercicio_clase_6.model.Estudiante;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        EstudianteController estudianteController = new EstudianteController(this);
+
+
+        ListView listView = findViewById(R.id.listaEstudiantes);
+
+        List<Estudiante> estudiantes = estudianteController.obtenerEstudiantes();
+        
+
         //setContentView(R.layout.activity_main_bd);
         setContentView (binding.getRoot());
-        binding.btnSaludar.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View view) {
-                binding.tvMessage.setText("Hola");
-            }
-        });
 
     }
 }
