0c98ce8b57267d36f034caab9c7f45ed1932402d
salo <salomontilla@outlook.com>
2025-04-01 22:11:13 -0500

    feat: crear el adapter para las notas

---
+++ app/src/main/java/com/example/ejercicio_clase_6/view/NotaListaAdapter.java
@@ -0,0 +1,49 @@
+package com.example.ejercicio_clase_6.view;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.example.ejercicio_clase_6.R;
+import com.example.ejercicio_clase_6.model.Nota;
+
+import java.util.List;
+
+public class NotaListaAdapter extends BaseAdapter {
+    private Context context;
+    private List<Nota> notas;
+
+    public NotaListaAdapter(Context context, List<Nota> notas) {
+        this.context = context;
+        this.notas = notas;
+    }
+
+    @Override
+    public int getCount() {
+        return notas.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return notas.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(context).inflate(R.layout.item_estudiante, viewGroup, false);
+        }
+        Nota nota = notas.get(i);
+        TextView notaView = convertView.findViewById(R.id.estudianteNota);
+        notaView.setText(String.valueOf(nota.getValor()));
+        return convertView;
+    }
+}
